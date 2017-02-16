243728fd154f172675217410b76485f90e6560ef
DivyaVipin <divyababu1001@gmail.com>
2017-02-16 19:23:18 +0530

    Initial commit

---
+++ app/src/main/java/assignment/android/acadgild/smsbroadcast/MainActivity.java
@@ -0,0 +1,20 @@
+package assignment.android.acadgild.smsbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
